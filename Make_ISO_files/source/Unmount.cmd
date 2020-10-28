f3ffbb91ee31f89212990781e2fe8c55d65ed41a
office-itou <itou-jun@outlook.com>
2020-10-28 13:14:38 +0900

    update

---
+++ Make_ISO_files/source/Unmount.cmd
@@ -0,0 +1,52 @@
+Rem ***************************************************************************
+@Echo Off
+    Cls
+
+Rem *** 作業開始 **************************************************************
+:START
+    Echo *** 作業開始 ******************************************************************
+    Echo %DATE% %TIME%
+
+    SetLocal EnableExtensions
+    SetLocal EnableDelayedExpansion
+
+Rem --- 作業環境確認 ----------------------------------------------------------
+    If /I "%USERNAME%" NEQ "Administrator" (
+        If /I "%SESSIONNAME%" NEQ "" (
+            Echo 管理者特権で実行して下さい。
+            GoTo DONE
+        )
+    )
+
+Rem --- 環境変数設定 ----------------------------------------------------------
+    For /F "usebackq delims=" %%I In (`Echo %0`) Do (
+        Set WRK_DIR=%%~dpI
+        Set WRK_DIR=!WRK_DIR:~0,-1!
+        Set WRK_FIL=%%~nxI
+        Set WRK_NAM=%%~nI
+    )
+
+    Set NOW_DAY=%date:~0,4%%date:~5,2%%date:~8,2%
+
+    If /I "%time:~0,1%" EQU " " (
+        Set NOW_TIM=0%time:~1,1%%time:~3,2%%time:~6,2%
+    ) Else (
+        Set NOW_TIM=%time:~0,2%%time:~3,2%%time:~6,2%
+    )
+
+    For /F "tokens=1-2 usebackq delims=\" %%I In ('!WRK_DIR!') Do (Set WRK_TOP=%%~I\%%~J)
+
+Rem *** Unmount ***************************************************************
+    TakeOwn /F "%~1\*.*" /A /R /D Y > NUL 2>&1
+    ICacls "%~1" /reset /T /Q
+    Dism /UnMount-Wim /MountDir:"%~1" /Discard
+
+Rem *** 作業終了 **************************************************************
+:DONE
+    CD "!CUR_DIR!"
+    EndLocal
+    Echo *** 作業終了 ******************************************************************
+    Echo %DATE% %TIME%
+    Echo [Enter]を押下して下さい。
+    Pause > Nul 2>&1
+    Echo On
